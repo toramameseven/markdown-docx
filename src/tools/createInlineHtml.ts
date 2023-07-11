0b214616e21b093415333af87a9fd3e812ad8854
toramameseven <toramameseven@com.com>
2023-07-11 19:33:46 +0900

    inline html

---
+++ src/tools/createInlineHtml.ts
@@ -0,0 +1,17 @@
+const { inlineSource } = require("inline-source");
+
+import * as path from "path";
+
+export async function createInlineHtml(htmlPath: string, htmlBody: string) {
+  try {
+    const r = await inlineSource(htmlBody, {
+      compress: false,
+      attribute: false,
+      rootpath: path.dirname(htmlPath),
+    });
+    return r as string;
+  } catch (error) {
+    console.log(error);
+  } 
+  return "";
+}
