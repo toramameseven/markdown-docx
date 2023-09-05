f0b9b81511ba79009a72a6f0b37cac73f1945503
toramameseven <toramameseven@com.com>
2023-09-05 19:07:03 +0900

    add non menu html

--- src/markdown-docx/tools/createInlineHtml.ts
+++ src/markdown-docx/tools/createInlineHtml.ts
@@ -5,26 +5,34 @@ const { inlineSource } = require("inline-source");
 import * as path from "path";
 import { getFileContents, selectExistsPath } from "./tools-common";
 
-export async function createInlineHtml(htmlPath: string, htmlBody: string) {
+export async function createInlineHtml(
+  htmlPath: string,
+  htmlBody: string,
+  withMenu: boolean
+) {
   try {
-    const r:string = await inlineSource(htmlBody, {
+    const r: string = await inlineSource(htmlBody, {
       compress: false,
       attribute: false,
       rootpath: path.dirname(htmlPath),
     });
 
-    const htmlTemplatePath = await selectExistsPath(
-      ["../../templates/sample.html", "../templates/sample.html"],
-      [__dirname]
-    );
+    if (withMenu) {
+      const htmlTemplatePath = await selectExistsPath(
+        ["../../templates/sample.html", "../templates/sample.html"],
+        [__dirname]
+      );
 
-    let sampleHtml = getFileContents(htmlTemplatePath);
+      let sampleHtml = getFileContents(htmlTemplatePath);
 
-    const outHtml = sampleHtml.replace("673ab4838a71448692a56d601b77c818", r);
+      const outHtml = sampleHtml.replace("673ab4838a71448692a56d601b77c818", r);
 
-    return outHtml;
+      return outHtml;
+    } else {
+      return r;
+    }
   } catch (error) {
-    throw(error);
+    throw error;
   }
   return "";
 }
