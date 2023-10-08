42626cb953badce8efd5ca4804b3c2efb0fb61e5
toramameseven <toramameseven@com.com>
2023-10-08 15:27:17 +0900

    add bookmarks ts for word

---
+++ src/markdown-docx/bookmarks.ts
@@ -0,0 +1,44 @@
+export class Bookmarks {
+  bookmarkMap = new Map();
+  constructor() {}
+
+  clear(){
+    this.bookmarkMap = new Map();
+  }
+
+  has(testId: string){
+    return this.bookmarkMap.has(testId);
+  }
+
+  slugify(header: string, alowDuplicate = false) {
+    //return encodeURI(
+    let r = header
+      .trim()
+      .toLowerCase()
+      .replace(
+        /[\]\[\!\"\#\$\%\&\'\(\)\*\+\,\.\/\:\;\<\=\>\?\@\\\^\_\{\|\}\~＠＃＄％＾＆＊（）＿＋－＝｛｝”’＜＞［］「」・、。～]/g,
+        ""
+      )
+      .replace(/\s+/g, "-") // Replace spaces with hyphens
+      .replace(/\-+$/, ""); // Replace trailing hyphen
+
+    if (alowDuplicate === false) {
+      r = this.createUniqId(r, r);
+    }
+
+    return r;
+  }
+
+  private createUniqId(id: string, originalId: string, index = 0) {
+    let testId = id;
+    if (this.bookmarkMap.has(testId)) {
+      testId = this.createUniqId(
+        originalId + "-" + (index + 1).toString(),
+        originalId,
+        index + 1
+      );
+    }
+    this.bookmarkMap.set(testId, testId);
+    return testId;
+  }
+}
